66db7b4431295196defe46aa1b5010eef6fec200
CukiD <40711562+Tektek9@users.noreply.github.com>
2024-04-07 17:00:30 +0700

    rfrw

--- rfid.ino
+++ rfid.ino
@@ -10,16 +10,18 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key defaultKey;
 
 int block = 2;
-byte readbackblock[18] = {0};
+byte readbackblock[18] = { 0 };
 String data;
 String newblockcontent;
+byte memberTag1[] = {0xD9, 0x3A, 0xE9, 0x56}; //Copy kan disini untuk array byte dari mode tag, dan aktifkan
+//byte memberTag2[] = {0x12, 0x34, 0x56, 0x78}; //Copy kan disini untuk array byte dari mode tag, dan aktifkan
+//byte memberTag3[] = {0x12, 0x34, 0x56, 0x78}; //Copy kan disini untuk array byte dari mode tag, dan aktifkan
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
   SPI.begin();
-  mfrc522.PCD_Init();
-  delay(4);
   initial();
 }
 
@@ -28,10 +30,10 @@ void loop() {
     data = Serial.readStringUntil('\n');
     int tanda = data.indexOf('=');
     if (tanda != -1) {
-      String mode = data.substring(0, tanda); 
+      String mode = data.substring(0, tanda);
       if (mode == "WRITE") {
         detectCard();
-        if (validate(block)) {
+        if (validate(block && isMember())) {
           String newblockcontent = data.substring(tanda + 1);
           writeCard(newblockcontent, block, readbackblock);
           ledSukses();
@@ -42,7 +44,7 @@ void loop() {
         stopDetect();
       } else if (mode == "VERIFY") {
         detectCard();
-        if (validate(block)) {
+        if (validate(block && isMember())) {
           readCard(block, readbackblock);
           ledSukses();
         } else {
@@ -51,9 +53,19 @@ void loop() {
         }
         stopDetect();
       }
-    } else {
+    } else if (data == "TAG") {
       detectCard();
       if (validate(block)) {
+        readTag();
+        ledSukses();
+      } else {
+        ledGagal();
+        Serial.println("Gagal4");
+      }
+      stopDetect();
+    } else if (data == "READ") {
+      detectCard();
+      if (validate(block) && isMember()) {
         readCard(block, readbackblock);
         ledSukses();
       } else {
@@ -67,6 +79,8 @@ void loop() {
 }
 
 void initial() {
+  mfrc522.PCD_Init();
+  delay(4);
   for (byte i = 0; i < 6; i++) {
     defaultKey.keyByte[i] = 0xFF;
   }
@@ -92,7 +106,68 @@ void ledSukses() {
   digitalWrite(LED_VALID, LOW);
 }
 
-//untuk input data ke kartu
+//penjaga
+bool hanSIP(byte uid[]) {
+  if (memcmp(uid, memberTag1, 4) == 0) {
+    return true;
+  }
+  return false;
+  // if (memcmp(uid, memberTag2, 4) == 0) {
+  //   return true;
+  // }
+  // return false;
+  // if (memcmp(uid, memberTag3, 4) == 0) {
+  //   return true;
+  // }
+  // return false;
+}
+
+//membaca informasi tag kartu rfid dan mengirim array byte yang nanti bisa digunakan untuk diisikan ulang di kode arduino ini untuk terdaftar sebagai member
+void readTag() {
+  Serial.print("Tag UID=");
+  String tagID = "";
+  String spasiID = "";
+  String penanda = "";
+
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    tagID += String(mfrc522.uid.uidByte[i], HEX);
+  }
+
+  for (int i = 0; i < tagID.length(); i += 2) {
+    spasiID += tagID.substring(i, i + 2);
+    spasiID += " ";
+  }
+
+  for (int i = 0; i < spasiID.length(); i++) {
+    spasiID[i] = toupper(spasiID[i]);
+  }
+
+  spasiID.trim();
+  Serial.print(spasiID);
+  Serial.print("={");
+  for (int x = 0; x < tagID.length(); x += 2) {
+    String byteID = tagID.substring(x, x + 2);
+
+    for (int y = 0; y < byteID.length(); y++) {
+      byteID[y] = toupper(byteID[y]);
+    }
+
+    if (byteID.length() == 1) {
+      byteID = "0" + byteID;
+    }
+
+    penanda += "0x" + byteID;
+
+    if (x + 2 < tagID.length()) {
+      penanda += ", ";
+    }
+  }
+
+  Serial.print(penanda);
+  Serial.println("}");
+}
+
+//Untuk input data ke kartu
 void writeCard(String newblockcontent, int blockNumber, byte arrayAddress[]) {
   byte newblockcontentBytes[newblockcontent.length() + 1];
   newblockcontent.getBytes(newblockcontentBytes, sizeof(newblockcontentBytes));
@@ -101,7 +176,7 @@ void writeCard(String newblockcontent, int blockNumber, byte arrayAddress[]) {
   }
 }
 
-//untuk membaca kartu
+//Untuk membaca kartu
 void readCard(int blockNumber, byte arrayAddress[]) {
   if (readBlock(blockNumber, arrayAddress)) {
     Serial.print("Data dari kartu=");
@@ -112,7 +187,7 @@ void readCard(int blockNumber, byte arrayAddress[]) {
   }
 }
 
-//untuk pendeteksian kartu pertama kali
+//Untuk pendeteksian kartu pertama kali
 void detectCard() {
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     return;
@@ -121,7 +196,19 @@ void detectCard() {
   delay(50);
 }
 
-//hansip rfid
+//Untuk cek member atau bukan
+bool isMember() {
+  if (hanSIP(mfrc522.uid.uidByte)) {
+    Serial.println("Kartu anda sudah terdaftar sebagai member");
+    return true;
+  } else {
+    Serial.println("Kartu anda belum terdaftar sebagai member");
+    Serial.println("Silahkan isikan lakukan mode tag dan isikan array byte dari hasil mode tag ke kode arduino");
+    return false;
+  }
+}
+
+//Validasi deteksi kartu rfid
 bool validate(int blockNumber) {
   int largestModulo4Number = blockNumber / 4 * 4;
   int trailerBlock = largestModulo4Number + 3;
@@ -147,7 +234,7 @@ bool writeBlock(int blockNumber, byte arrayAddress[]) {
 
 //Read specific block
 bool readBlock(int blockNumber, byte arrayAddress[]) {
-  byte buffersize = 18;                         
+  byte buffersize = 18;
   byte status2 = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);
   if (status2 != MFRC522::STATUS_OK) {
     Serial.print("MIFARE_read() failed: ");
